ca0169fe2e068412c41f590231c99fbfcf2a754d
ryan0980 <Ryan0980@outlook.com>
2024-04-16 17:49:59 -0400

    tru more room for reward, but not work well

--- AWS_DeepRacer/20240414_AWS_DeepRacer.py
+++ AWS_DeepRacer/20240414_AWS_DeepRacer.py
@@ -9,16 +9,15 @@ def reward_function(params):
     progress = params['progress']
 
     # 如果车辆不在赛道上或已经碰撞，则返回最小奖励
-    if not all_wheels_on_track or is_crashed:
-        reward = 1e-3
-        return float(reward)
+    if not all_wheels_on_track or params['is_reversed'] or params['is_offtrack'] or params['is_crashed']:
+        return 1e-3
 
     reward = 5.0  # 初始奖励
 
     # 根据车辆距离赛道中心的远近调整奖励
-    outer_radius = track_width / 2.0
-    mid_radius = track_width / 4.0
-    inner_radius = track_width / 8.0
+    outer_radius = track_width*0.8
+    mid_radius = track_width*0.5
+    inner_radius = track_width*0.25
 
     if distance_from_center <= inner_radius:
         reward += 1.0
@@ -31,22 +30,18 @@ def reward_function(params):
         reward += 1.0 if speed > 3 else 0.5
 
     # 考虑车辆的转向角度和速度的组合
-    if abs(steering_angle) > 20:
-        if speed > 3:
-            reward += 1.0
+    ABS_STEERING_THRESHOLD = 18.0
+    if abs(steering_angle) > ABS_STEERING_THRESHOLD:
+        if speed > 1.5:
+            reward += 0.8
         else:
-            reward += 1.5
-    else:
-        if speed > 3.5:
-            reward += 4.0  # 最高速度区间，给予最高奖励
-        elif speed > 3.0:
-            reward += 2.5
-        elif speed > 2.5:
-            reward += 1.8
-        elif speed > 2.0:
-            reward += 1.3
-        else:
-            reward += 0.1
+            reward -= 0.5  # 转弯时速度太慢则惩罚
+    if speed > 3:
+        reward += 2.0
+    elif speed > 2:
+        reward += 1.0
+    elif speed > 1:
+        reward += 0.5
 
     # 可以根据需要启用以下注释掉的代码段
     '''
@@ -68,10 +63,8 @@ def reward_function(params):
     '''
 
     # 基于进度和步数的效率奖励
-    if steps > 0:
-        progress_ratio = progress / 100
-        efficiency = progress_ratio / steps
-        step_reward = efficiency * 40
-        reward += step_reward
+    if steps > 0 and progress > 0:
+        efficiency = progress / steps
+        reward += efficiency * 5.0
 
     return float(reward)
